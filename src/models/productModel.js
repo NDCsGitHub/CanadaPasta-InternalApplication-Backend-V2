6338492a0894c23a1de5d31069f09bce87cd947d
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2022-06-07 13:19:59 -0400

    ss

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -18,35 +18,3 @@ const productSchema = mongoose.Schema({
 module.exports = mongoose.model('product', productSchema)
 
 
-
-    `// define models, product schema`
-
-    `const mongoose = require('mongoose')`
-
-    `// product Schema definition`
-
-    `const productSchema = mongoose.Schema({`
-
-    `Product_Name_EN: {`
-
-    `type: String,`
-
-    `required: [true, 'Please add Product Name EN']`
-
-    `},`
-
-    `Product_Name_CN: {`
-
-    `type: String,`
-
-    `required: [true, 'Please add Product Name CN']`
-
-    `}`
-
-    `}, {`
-
-    `timestamps: true,`
-
-    `})`
-
-    `module.exports = mongoose.model('product', productSchema)`
