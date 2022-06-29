2e5db13e8757bf203384b4ff44ef5ebf64548111
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2022-06-28 21:32:22 -0400

    ss

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -3,6 +3,21 @@ const router = express.Router()
 
 
 // import controllers
+const {
+    getCustomer,
+    setCustomer,
+
+} = require('../controllers/customerController')
+
+
+// import private route middleware
+const { protect } = require('../middlewares/authMiddleware')
 
 
 
+// routes for customer
+router.route('/').get(protect, getCustomer).post(protect, setCustomer)
+router.route('/').get(protect, updateCustomer).delete(protect, deleteCustomer)
+
+
+module.exports = router
