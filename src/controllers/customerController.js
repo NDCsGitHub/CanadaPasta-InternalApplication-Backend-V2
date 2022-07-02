7d5e509f827d83e11ddc66f7af010abb028cab21
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2022-07-02 16:36:51 -0400

    ss

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler')
 // import model/schema
 const UserModel = require('../models/userModel')
 const CustomerModel = require('../models/customerModel')
-const { default: customerService } = require('../../../CanadaPasta-InternalApplication-Frontend-V2/src/features/customer/customerService')
+
 
 
 
@@ -27,4 +27,56 @@ const getCustomer = asyncHandler(async (req, res) => {
 })
 
 
+// @desc Get All Customers base on the user who created it
+// @route GET /api/customers
+// @access Private
+const getUserCustomers = asyncHandler(async (req, res) => {
 
+    // find all the customers base creator
+    const customers = await CustomerModel.find({ user: req.user.id })
+    res.status(200).json({
+        message: 'Get all customers base on the creator',
+        error: false,
+        data: customers,
+    })
+})
+
+
+
+/*********************************CREATE***********************************/
+// @desc Create Customer
+// @route POST /api/customers
+// @access Private
+const setCustomer = asyncHandler(async (req, res) => {
+
+    if (!req.body.product_name_en) {
+        res.status(400)
+        throw new Error('Please add product name')
+    }
+
+    // check for user
+    if (!req.user) {
+        res.status(401)
+        throw new Error('A user must login to create the products')
+    }
+
+    // add new product
+    const product = await CustomerModel.create({
+
+    })
+    res.status(200).json({
+        message: 'Product Added',
+        error: false,
+        data: product,
+    })
+})
+
+
+
+
+
+module.exports = {
+    getCustomer,
+    getUserCustomers,
+    setCustomer,
+}
--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-
 // import controllers
 const {
     getCustomer,
+    getUserCustomers,
     setCustomer,
 
 } = require('../controllers/customerController')
@@ -19,11 +19,11 @@ const { protect } = require('../middlewares/authMiddleware')
 // @desc routes for customer
 // @route /api/customers
 router.route('/').get(protect, getCustomer).post(protect, setCustomer)
-router.route('/').put(protect, updateCustomer).delete(protect, deleteCustomer)
+// router.route('/').put(protect, updateCustomer).delete(protect, deleteCustomer)
 
 // @desc routes for customer base on user
 // @route /api/customers
-router.route('/usercustomer').get(protect, getCustomer).post(protect, setCustomer)
+// router.route('/usercustomer').get(protect, getCustomer).post(protect, setCustomer)
 
 
 
